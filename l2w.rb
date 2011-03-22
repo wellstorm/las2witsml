862a7a191a7e7f33801c3f0156d31c3a50175b7d
Hugh Winkler <hugh.winkler@wellstorm.com>
2011-03-21 19:03:19 -0500

    checkpoint.

--- l2w.rb
+++ l2w.rb
@@ -1,19 +1,16 @@
 #!/usr/bin/ruby 
-require 'las2witsml'
 
- l2w = Las2Witsml.new
- infile = File.new 'asphulto.las', 'r'
- outfile = File.new "asphaulto.witsml", "w"
- l2w.run infile, outfile, 't1', 'b1', 'l1', 'test log'
 
-# 
-#  infile = File.new 'md.las', 'r'
-#  outfile = File.new 'md.witsml', 'w'
-#  l2w.run infile, outfile, 't1', 'b1', 'l1', 'test log'
-# 
-# 
-# infile = File.new 'dt.las', 'r'
-# outfile = File.new 'dt.witsml', 'w'
-# l2w.run infile, outfile, 't1', 'b1', 'l1', 'test log'
+require "#{File.dirname(__FILE__)}/las_file"
+require "#{File.dirname(__FILE__)}/witsml_file"
+require "#{File.dirname(__FILE__)}/las2witsml"
+
+ARGV.each do|a|
+  l2w = Las2Witsml.new
+  infile = File.new a, 'r'
+  outfile = $stdout
+  l2w.run infile, outfile, 't1', 'b1', 'l1', 'test log'
+end
+
 
 
