6615c3a5d4d1c37c3257b48b642d7c6554af2882
Hugh Winkler <hugh.winkler@wellstorm.com>
2012-06-12 18:34:24 -0500

    gemify

--- las2witsml.gemspec
+++ las2witsml.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
   s.email = %q{hugh.winkler@wellstorm.com}
   s.summary = %q{Converts Log ASCII Standard (LAS) files to WITSML <log> objects.}
   s.homepage = %q{https://github.com/wellstorm/las2witsml/}
-  s.description = %q{Converts Log ASCII Standard (LAS) files to WITSML <log> objects.}
-  s.files = [ "README", "LICENSE", "lib/las2witsml.rb"]
+  s.description = %q{A command line tool and a library for converting Log ASCII Standard (LAS) files to WITSML <log> objects.}
+  s.files = [ "README", "LICENSE", "lib/las2witsml.rb", "lib/las_file.rb", "lib/witsml_file.rb"]
   s.executables = [ "las2witsml" ]
 end
--- lib/las2witsml.rb
+++ lib/las2witsml.rb
@@ -2,14 +2,14 @@
 #require 'las_file'
 #require 'witsml_file'
 class Las2Witsml
-  def run in_stream, out_stream, uid_well, uid_wellbore, uid_log, name_log, verbose=false
+  def run in_stream, out_stream, uid_well, uid_wellbore, uid_log, name_log, version = 1411, verbose=false
     begin
  
       las =LasFile.new in_stream 
       las.process verbose
       $stderr.puts 'processed las file'
       
-      witsml = WitsmlFile.new out_stream
+      witsml = WitsmlFile.new out_stream, version
       $stderr.puts 'made witsmlFile' if verbose
       witsml.from_las_file(las, uid_well, uid_wellbore, uid_log, name_log,  verbose)   
       $stderr.puts 'read from lasFile' if verbose
